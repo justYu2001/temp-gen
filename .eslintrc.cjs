a895be7564e3afc6c45efa360a6876d17825d9db
yenyu-chen <yenyu.chen@linecorp.com>
2024-08-29 18:33:03 +0800

    chore: setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+/** @type {import("eslint").Linter.Config} */
+
+const config = {
+  plugins: ["@typescript-eslint", "typescript-sort-keys", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    projectService: true,
+  },
+  root: true,
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", fixStyle: "inline-type-imports" },
+    ],
+    "import/consistent-type-specifier-style": ["error", "prefer-inline"],
+  },
+};
+
+module.exports = config;
