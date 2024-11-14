bd84471f51c1d6933fb7f2c58ed492ce02faedfe
justYu2001 <jack4781012@gmail.com>
2024-11-14 12:26:29 +0800

    feat: set import alias

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,8 +3,23 @@ import { Command } from "commander";
 
 import { validateProjectName } from "@/cli/validateProjectName.js";
 import { CLI_TOOL_NAME, DEFAULT_PROJECT_NAME } from "@/constants.js";
+import { validateImportAlias } from "./validateImportAlias.js";
 
-export const runCli = () => {
+interface CliResult {
+  projectName: string;
+  flags: {
+    importAlias: string;
+  };
+}
+
+const DEFAULT_CLI_RESULT: CliResult = {
+  projectName: DEFAULT_PROJECT_NAME,
+  flags: {
+    importAlias: "@/",
+  },
+};
+
+export const runCli = async () => {
   const command = new Command()
     .name(CLI_TOOL_NAME)
     .description("A CLI for creating JS/TS project template generator.")
@@ -12,22 +27,41 @@ export const runCli = () => {
       "[directory]",
       "The name of the generator, as well as the name of the directory to create",
     )
+    .option(
+      "-i, --import-alias <alias>",
+      "Explicitly tell the CLI to use a custom import alias",
+      DEFAULT_CLI_RESULT.flags.importAlias,
+    )
     .parse();
 
   const projectNameFromCommand = command.args[0];
-  if (projectNameFromCommand) {
-    return {
-      projectName: projectNameFromCommand,
-    };
-  }
 
-  return p.group({
-    projectName: () => {
+  const flagsFromCommand = command.opts<CliResult["flags"]>();
+
+  const interactiveCliResult = await p.group({
+    ...(!projectNameFromCommand && {
+      projectName: () => {
+        return p.text({
+          message: "What will your project be called?",
+          defaultValue: DEFAULT_PROJECT_NAME,
+          validate: validateProjectName,
+        });
+      },
+    }),
+    importAlias: () => {
       return p.text({
-        message: "What will your project be called?",
-        defaultValue: DEFAULT_PROJECT_NAME,
-        validate: validateProjectName,
+        message: "What import alias would you like to use?",
+        defaultValue: flagsFromCommand.importAlias,
+        placeholder: flagsFromCommand.importAlias,
+        validate: validateImportAlias,
       });
     },
   });
+
+  return {
+    projectName: projectNameFromCommand ?? interactiveCliResult.projectName!,
+    flags: {
+      importAlias: interactiveCliResult.importAlias,
+    },
+  };
 };
